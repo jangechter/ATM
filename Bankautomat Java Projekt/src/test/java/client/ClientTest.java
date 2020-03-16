0765a248002847644b61cf49ddf0a99c250c53ea
Jan Gechter <jan.gechter@valtech.com>
2020-03-16 12:57:09 +0100

    Testdaten angepasst

--- Bankautomat Java Projekt/src/test/java/client/ClientTest.java
+++ Bankautomat Java Projekt/src/test/java/client/ClientTest.java
@@ -8,7 +8,6 @@
 
 package client;
 
-import java.io.File;
 import java.math.BigDecimal;
 import java.util.ResourceBundle;
 
@@ -20,14 +19,11 @@ public class ClientTest {
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("TestData");
     private static final String TEST_NAME = RESOURCE_BUNDLE.getString("name");
-    private static final double TEST_BANK_BALANCE = 100.00;
+    private static final double TEST_BANK_BALANCE = Double.parseDouble(RESOURCE_BUNDLE.getString("bankBalance"));
     private static final String TEST_FIRSTNAME = RESOURCE_BUNDLE.getString("firstname");
     private static final String TEST_IBAN = RESOURCE_BUNDLE.getString("de01.2345.6789.0123.4567.89");
     private static final String TEST_PIN = RESOURCE_BUNDLE.getString("pin");
     private static final boolean IS_ACTIVE = Boolean.parseBoolean(RESOURCE_BUNDLE.getString("status"));
-
-    private static final File TEST_FILE = new File(System.getProperty("user.dir") + "/src/test/resources/" +
-                                                   TEST_IBAN + ".csv");
     private static final Client TEST_CLIENT = new Client(TEST_NAME, TEST_FIRSTNAME, TEST_IBAN, TEST_PIN, BigDecimal
             .valueOf(TEST_BANK_BALANCE), IS_ACTIVE);
 
